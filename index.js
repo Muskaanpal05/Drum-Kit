f84a24ecf2c01adc91dd87bf144c59c114970708
Muskaanpal05 <palmuskaan5@gmail.com>
2020-09-08 16:37:14 +0530

    Changing the color of the text

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 
     function handleClick() {
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+        this.style.color = "white";
     }
 }
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
